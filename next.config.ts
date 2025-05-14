b414edd92b7f762e27b5e168008b7fe15de5fdff
Blanc <topblade6@naver.com>
2025-05-14 15:35:59 +0900

    fix: vercel.json 제거

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: false,  
+  },
   env: {
     API_BASE_URL: process.env.API_BASE_URL
   }
